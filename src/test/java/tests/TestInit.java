346dcc61279131ecb8515af347c3c80a5799a32b
Oleksandr Kraglyk <sanek2070452@gmail.com>
2023-12-06 13:31:11 +0200

    updated AfterMethod for allure screenshots

--- src/test/java/tests/TestInit.java
+++ src/test/java/tests/TestInit.java
@@ -1,12 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Allure;
 import listeners.TestListener;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+
+import java.io.ByteArrayInputStream;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.closeWindow;
@@ -25,12 +32,22 @@ public class TestInit extends Assert {
     }
 
     @AfterMethod
-    public void closeTest() {
+    public void closeTest(ITestResult result) {
+        if (result.getStatus() == ITestResult.SKIP || result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot screenshotDriver = (TakesScreenshot) WebDriverRunner.getWebDriver();
+            byte[] screenshot = screenshotDriver.getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment("Test" + result.getMethod().getMethodName(), "image/png",
+                    new ByteArrayInputStream(screenshot), "png");
+        }
         closeWindow();
     }
 
     public static boolean getUrl(String endpoint) {
         return WebDriverRunner.getWebDriver().getCurrentUrl().contains(endpoint);
+    }
+
+    public static void refreshPage() {
+        Selenide.refresh();
     }
 }
 
